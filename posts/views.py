79c31db75eefb439272ceac421ae095f5abcd7c2
braian <braianyanquin@gmail.com>
2022-07-31 12:31:57 -0300

    Blog API setup

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .serializers import PostSerializer
+from .models import Post
+
 
 # Create your views here.
+
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
