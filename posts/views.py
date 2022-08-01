e4c7d6af52728b4e9b61060d2efb8a24a24c3259
braian <braianyanquin@gmail.com>
2022-08-01 12:55:22 -0300

    More on permissions and viewsets added

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from django.contrib.auth import get_user_model
 
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .models import Post
 
 
 # Create your views here.
 
-class PostList(ListCreateAPIView):
+class PostViewSet(ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+class UserViewSet(ModelViewSet):
+    permission_classes = [IsAdminUser,]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
