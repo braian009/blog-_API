4817598b146cd0761f9ea3d66ad8efa86f6fc6a3
braian <braianyanquin@gmail.com>
2022-08-02 15:10:28 -0300

    API schema and documentation added

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'dj_rest_auth.registration',
-
+    'drf_spectacular',
 
     # local
     'accounts.apps.AccountsConfig',
@@ -149,7 +149,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
 
 CORS_ORIGIN_WHITELIST = (
@@ -162,3 +163,10 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Blog API Project',
+    'DESCRIPTION': 'A sample blog to learn about DRF',
+    'VERSION': '1.0.0', 
+    # OTHER SETTINGS
+}
